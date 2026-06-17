f126e487d5c60fef080c25f12931119fcea2f193
prasad96922 <prasadthaduka@gmail.com>
2026-06-17 23:27:43 +0530

    Module 7.1 and 7.2 : spring boot Testing : JUnit (annotations) and Assertions

---
+++ src/test/java/com/Lvprasad/SecurityAppApplication/SecurityApp/CommonJunitAnnotations.md
@@ -0,0 +1,17 @@
+
+**** Common JUNIT Annotations
+1. @Test
+2. @DisplayName
+3. @Disabled
+
+**** Some Other Useful JUnit Annotation in day-to-day life cycle
+
+1. @BeforeEach
+2. @AfterEach
+3. @BeforeAll- run once use static method
+4. @AfterAll- run once use static method
+
+JUnit Vs AssertJ
+
+1. JUnit: one of the most widely used testing frameworks in the java ecosystem
+2. AssertJ: maintain
--- src/test/java/com/Lvprasad/SecurityAppApplication/SecurityApp/SecurityAppApplicationTests.java
+++ src/test/java/com/Lvprasad/SecurityAppApplication/SecurityApp/SecurityAppApplicationTests.java
@@ -1,30 +1,115 @@
 package com.Lvprasad.SecurityAppApplication.SecurityApp;
 
 import com.Lvprasad.SecurityAppApplication.SecurityApp.entities.UserEntity;
+import com.Lvprasad.SecurityAppApplication.SecurityApp.entities.enums.Role;
 import com.Lvprasad.SecurityAppApplication.SecurityApp.services.JwtService;
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.AbstractBigDecimalAssert;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.*;
+import org.assertj.core.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Set;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 
 @SpringBootTest
 //@RequiredArgsConstructor
+@Slf4j
 class SecurityAppApplicationTests {
 
     @Autowired
     private JwtService jwtService;
 
+    @BeforeEach
+    void BeforeEach(){
+        log.info("starting the method, setting up configuration: BeforeEach");
+    }
+
+    @AfterEach
+    void AfterEach(){
+        log.info("AfterEach method, setting up configuration or tearing Out: AfterEach");
+    }
+
+    @BeforeAll
+    static void beforeAll(){
+        log.info("BeforeAll");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        log.info("AfterAll");
+    }
+
+    int addTwoNumbers(int a, int b){
+        return a+b;
+    }
+
+
     @Test
-    void contextLoads() {
-        UserEntity user = new UserEntity(2L, "vara@gmail.com", "vara#123", "vara");
+    void testNumberOne() {
+//        log.info("Test one is run");
+        int numberOne = addTwoNumbers(1,2);
+//        Assertions.assertEquals(3,numberOne);
+
+//        Assertions.assertThat(numberOne)
+//                .isEqualTo(3)
+//                .isCloseTo(3, Offset.offset(1));
+
+        assertThat("Apple").isEqualTo("Apple").startsWith("App").endsWith("le").hasSize(5);
+    }
+
+
+
+
+    @Test
+    @DisplayName("displayTestNameTwo")
+    void testNumberTwo() {
+        log.info("Test two is run");
+    }
+
+
+
+    @Test
+    @Disabled
+    void contextLoadsTest() {
+
+        UserEntity user = new UserEntity(
+                20L,
+                "prasad@gmail.com",
+                "Prasad#123",
+                "prasad",
+                Set.of(Role.USER)
+        );
 
         String token = jwtService.generateAccessToken(user);
-        System.out.println("Token ID==============>" + token);
+        System.out.println("Token = " + token);
 
         Long userId = jwtService.getUserIdFromToken(token);
-        //Error:[JWT expired 451280 milliseconds ago at 2026-06-07T04:42:13.000Z. Current time: 2026-06-07T04:49:44.280Z. Allowed clock skew: 0]
-        System.out.println("User ID==============>" + userId);
+        System.out.println("User ID = " + userId);
+    }
 
+    @Test
+    @Disabled
+    void BuilderContextLoads() {
+
+        UserEntity user = UserEntity.builder()
+                .id(2L)
+                .email("vara@gmail.com")
+                .password("vara#123")
+                .name("vara")
+                .roles(Set.of(Role.USER))
+                .build();
+
+        String token = jwtService.generateAccessToken(user);
+
+        System.out.println("Token = " + token);
+
+        Long userId = jwtService.getUserIdFromToken(token);
+
+        System.out.println("User ID = " + userId);
     }
 }
